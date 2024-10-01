a02a283e7055bf710a02afd7d90df4078bc364dd
GitcrackerUK <pfsiwek@gmail.com>
2024-10-01 11:18:20 +0100

    refactored function

--- routes/WorkTracker/factory/createCalendar.js
+++ routes/WorkTracker/factory/createCalendar.js
@@ -24,13 +24,9 @@ function getNameOfWeekDay(payload, i) {
     return moment(payload).date(i).format('dddd');
 }
 
-function extractDateFromString(str){
-    let date = [];
-    str.split(/[/" ":]/).forEach((i)=>{
-        date.push(parseInt(i))
-    })
-    return date
-};
+function extractDateFromString(str) {
+    return str.split(/[/" ":]/).map(i => parseInt(i));
+}
 
 function returnDate(dateArg, extractDateFromString, day, startTime){
     let payload = [];
